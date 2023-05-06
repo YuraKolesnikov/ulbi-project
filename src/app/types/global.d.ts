1865296e10ec671cbf1efb2c60186a493f3a6050
YuraKolesnikov <jurijs_kolesnikovs@inbox.lv>
2023-05-06 19:57:21 +0700

    Added svg/file loaders

--- src/app/types/global.d.ts
+++ src/app/types/global.d.ts
@@ -7,7 +7,11 @@ declare module '*.scss' {
   export = classNames
 }
 
+declare module '*.png'
+declare module '*.jpg'
+declare module '*.jpeg'
 declare module '*.svg' {
-  const content: any
-  export default content
+  import React from 'react'
+  const SVG: React.VFC<React.SVGProps<SVGSVGElement>>
+  export default SVG
 }
--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,22 +1,22 @@
-import { FC } from 'react'
+import { ButtonHTMLAttributes, FC } from 'react'
 import classNames from 'classnames'
 
 import { useTheme } from 'app/providers/ThemeProvider'
-import { Theme } from 'app/providers/ThemeProvider/lib/ThemeContext'
+import { Theme } from 'app/providers/ThemeProvider'
 
 import LightIcon from 'shared/assets/icons/theme-light.svg'
 import DarkIcon from 'shared/assets/icons/theme-dark.svg'
 
 import css from './ThemeSwitcher.module.scss'
 
-interface IThemeSwitcherProps {
+interface IThemeSwitcherProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string
 }
 
 export const ThemeSwitcher: FC<IThemeSwitcherProps> = ({ className }) => {
   const { theme, toggleTheme } = useTheme()
   return (
-    <button className={classNames(css.btn, className)} onClick={toggleTheme}>
+    <button className={classNames(css.Button, className)} onClick={toggleTheme}>
       {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
     </button>
   )
