0fa92b038ec2aa2c005332d29e635d05c544dd09
YuraKolesnikov <jurijs_kolesnikovs@inbox.lv>
2023-06-19 20:18:05 +0300

    Added dynamic lang to translate btn

--- src/widgets/lang-switcher/ui/lang-switcher.tsx
+++ src/widgets/lang-switcher/ui/lang-switcher.tsx
@@ -4,7 +4,7 @@ import { Button } from 'shared/ui'
 import { useTranslation } from 'react-i18next'
 
 export const LangSwitcher: FC = ({}) => {
-  const { i18n } = useTranslation()
+  const { t, i18n } = useTranslation()
 
   const currentLang = useMemo(() => i18n.language, [i18n.language])
 
@@ -12,5 +12,5 @@ export const LangSwitcher: FC = ({}) => {
     i18n.changeLanguage(currentLang === 'ru' ? 'en' : 'ru')
   }, [currentLang])
 
-  return <Button onClick={onLangToggle}>{currentLang === 'ru' ? 'English' : 'Russian'}</Button>
+  return <Button onClick={onLangToggle}>{t('ChangeLang')}</Button>
 }
