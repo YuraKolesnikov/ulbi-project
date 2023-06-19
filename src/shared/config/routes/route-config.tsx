262014f29ae22d0814d517ce0345aa3980da289b
YuraKolesnikov <jurijs_kolesnikovs@inbox.lv>
2023-06-19 23:44:01 +0300

    Added 404 page

--- src/shared/config/routes/route-config.tsx
+++ src/shared/config/routes/route-config.tsx
@@ -2,24 +2,31 @@ import { type RouteProps } from 'react-router-dom'
 
 import { HomePage } from 'pages/home'
 import { AboutPage } from 'pages/about'
+import { NotFoundPage } from 'pages/not-found-page'
 
 export enum AppRoutes {
   HOME = 'home',
-  ABOUT = 'about'
+  ABOUT = 'about',
+  NOT_FOUND = 'not_found'
 }
 
 export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.HOME]: '/',
-  [AppRoutes.ABOUT]: '/about'
+  [AppRoutes.ABOUT]: '/about',
+  [AppRoutes.NOT_FOUND]: '*',
 }
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.HOME]: {
     path: RoutePath.home,
-    element: <HomePage/>
+    element: <HomePage/>,
   },
   [AppRoutes.ABOUT]: {
     path: RoutePath.about,
-    element: <AboutPage/>
-  }
+    element: <AboutPage/>,
+  },
+  [AppRoutes.NOT_FOUND]: {
+    path: RoutePath.not_found,
+    element: <NotFoundPage/>,
+  },
 }
